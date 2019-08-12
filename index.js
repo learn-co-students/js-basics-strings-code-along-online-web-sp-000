0b5bc5cf92bb698c2d48e8b8cf6c63259a7153c9
Ariel <arielord@buffalo.edu>
2019-08-12 04:53:55 +0000

    Done.

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 const currentUser = "Grace Hopper";
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = `Welcome, ${currentUser.splice(0,1)}`;
+const shortGreeting = `Welcome, ${currentUser.splice(0,1)}!`;

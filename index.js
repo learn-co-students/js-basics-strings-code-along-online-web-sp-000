e784941a4e900b28db385734bc10ea60b84c0c2e
Ariel <arielord@buffalo.edu>
2019-08-12 04:52:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 // Write your code in this file!
 const currentUser = "Grace Hopper";
-const welcomeMessage = "Welcome to Flatbook, " + currentUser;
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.splice(0,1)}`;

09275e752618b6b3569c78b852e4b482b8c90d61
ch2isk4kos <ch2isk4kos@gmail.com>
2019-09-06 17:10:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+
+// set currentUser to "Chris" that can be changed at a later time.
+let currentUser = "Chris"
+
+// set welcomeMessage to include greeting and currentUser
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
+
+// set excitedWelcomeMessage to include greeting and currentUser in all caps.
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!"
+
+// set shortGreeting to include greeting and first initial of currentUser.
+const shortGreeting = "Welcome, " + currentUser.charAt(0) + "!"

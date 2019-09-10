93549b4a9f2f6b8e31219d6ddd7e86a1863aca97
github username <github email address>
2019-09-10 13:01:01 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code in this file!
+const currentUser = 'Kelly Dobbins';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+//how i originally done it:
+// const shortGreeting = 'Welcome, ' + currentUser[0] + '!';
+//this passed the test but to make the code more flexible for future use. Need to use the slice() method$.ajax({
+ 
+const slicedUser = currentUser.slice(0,1)
+const shortGreeting = `Welcome, ${slicedUser}!`
+
+//that returns: "Welcome, K!"
